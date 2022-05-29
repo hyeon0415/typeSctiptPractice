4f8d3a9395f59ac32237f92d913f8efd6440e25f
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2022-05-29 23:31:16 +0900

    recap

--- memo3.ts
+++ memo3.ts
@@ -19,3 +19,37 @@ class Player3 extends User {
 }
 
 const nico3 = new Player3("nico", "las", "니꼬")
+
+//hashMap 
+type Words = {
+    [key:string]: string 
+}
+
+
+class Dict {
+    private words: Words
+    constructor() {
+        this.words = {}
+    }
+    add(word:Word){
+        if(this.words[word.term] === undefined){
+            this.words[word.term] === word.def;
+        }
+    }
+    def(term){
+        return this.words
+    }
+}
+
+class Word {
+    constructor(
+        public term:string,
+        public def:string 
+    ){}
+}
+
+const kimchi = new Word("kimchi", "한국의 음식");
+
+const dict = new Dict()
+dict.add(kimchi);
+dict.def("kimchi");
