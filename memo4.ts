2e7b06ffcd78265409271625f4b81e2541670119
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2022-06-01 00:04:47 +0900

    2022. 05, 31
    2022 interFace

---
+++ memo4.ts
@@ -0,0 +1,18 @@
+type NickName = string 
+type Health = number
+type Friends = Array<string>
+
+type Player4 = {
+    nickname:string,
+    healthbar: number,
+
+}
+
+const nico4 : Player4 = {
+    nickname:"nico",
+    healthbar:10
+}
+
+type Food = string;
+
+const kimchi2:Food = "delecious";
