efec55620e978a04a7525285ecb99062667b5220
hyeon0415 <82595578+hyeon0415@users.noreply.github.com>
2022-06-14 23:42:12 +0900

    block
    2022. 06. 14

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-import {exit, init} from "./myPackage";
+import crypto from "crypto";
 
+interface BlockShap {
+    hash:string;
+    prevHash:string;
+    height:number;
+    data:string;
+}
+
+class Block implements BlockShap{
+    public hash: string;
+    constructor(
+        public prevHash:string,
+        public height:number,
+        public data: string 
+    ){
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash:string, height:number, data:string){
+        const toHash = `${prevHash}${height}${data}`
+    }
+}
 
